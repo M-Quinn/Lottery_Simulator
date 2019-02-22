09da9ca2b1e0504516302db197d99c2af860d1ca
M-Quinn <qunnmicha@gmail.com>
2019-02-21 23:32:14 -0500

    Changed the intro and other format
    Things Changed:
    Intro was heavily changed with the addition of an actual intro suitable for the user
    The menu was also changed with descriptions of the simulations
    A small formating in the unlimited simulation where "games played" was changed to "tickets purchased"

--- PlayLotto.py
+++ PlayLotto.py
@@ -13,15 +13,28 @@ wNumbers=[0,0,0,0,0,0]     #List holds the Winning numbers
 price=0         #How much money spent on playing the powerball
 match=0         #counts how many numbers match to win lotto
 highestMatch=0    #counts the closest to 5 matches achieved
+amtHighest=0    #counts the amount of times user hit the highestMatch
 x=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69']
 n=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26']
 #------------Functions------------------------
 def intro():
-    print('You are 18 years old')
-    print('If you played the Powerball every week,\n\twould you be able to win?')
-    print('\nTo make life easy choose your lucky numbers now.')
-    print('\nYou can not repeat numbers')
-    print('You can only choose a number between 1-69\n')
+    print('\t ________________________________________________ ')
+    print('\t|                                                |')
+    print('\t|                                                |')
+    print('\t|            Lottery Simulator                   |')
+    print('\t|                                                |')
+    print('\t|  By MQ                                         |')
+    print('\t|                                                |')
+    print('\t|________________________________________________|')
+    input('Press "enter" to continue...')
+    print('\n\tHave you ever seen someone win the lottery and think,"Well if I only bought ticket..."')
+    print('\n\tThis simulator will let you virtually buy one ticket a week with your lucky numbers.')
+    input('\nPress "enter" to continue...')
+    print('\n\n\tThis simulator uses real Powerball rules.\n\tYou can not repeat numbers,')
+    print('\tYou choose 5 numbers between 1-69. Then you choose the powerball number 1-26.')
+    print('\tYou can not repeat any of the 5 regular numbers. The powerball number can match one of your 5 regular numbers.')
+    print('\n\n\tEach ticket costs $2.00\n\tIf you match all five numbers you win $1 Million, If you also match the powerball number you win $324Million.\n')
+    input('\nPress "enter" to pick your lucky numbers...')
 
 def chosenNumbers():
     while numbers[0] not in x:#this checks the manually created list of numbers in string form to make sure the number chosen is within range
@@ -60,7 +73,10 @@ def chosenNumbers():
 
 def menu():
     print('1.) Can I win the Powerball in my lifetime')
-    print('2.) How long would it take to win? **Warning**This may take up to 20minutes')
+    print('\t\tYou are 18 years old')
+    print('\t\tIf you played the Powerball once every week until you were 80,\n\t\twould you be able to win?\n')
+    print('2.) How long would it take to win? **Warning**This may take 2+ hours')
+    print('\t\tSimulation ends when you win the lottery\n\t\tInfo displayed: Years spent, Money Spent, Amount of tickets purchased, and closest chance to winning')
     choice=0
     while choice!=1 and choice!=2:
         choice=int(input('\n1 or 2 :'))
@@ -91,7 +107,7 @@ print(f'Your numbers are {numbers[0]} {numbers[1]} {numbers[2]} {numbers[3]} {nu
 choice=menu()
 if choice==1:
 
-    input('Hit "enter" to start the simulation')
+    input('Hit "enter" to start the simulation...')
     while week>0 and lotto==0:
         week-=1
         wNumbers=winningNumbers()
@@ -106,6 +122,9 @@ if choice==1:
         else:
             if match>highestMatch:
                 highestMatch=match
+                amtHighest=1
+            elif match==highestMatch:
+                amtHighest+=1
 
     if lotto==1:
         if numbers[5]==wNumbers[5]:
@@ -117,18 +136,15 @@ if choice==1:
             print(f'You get 1 Million dollars and it only cost you {age} years and ${price} dollars')
     else:
         print(f'After playing 3,224 games and spending ${price} you never won the lottery......')
-        print(f'The closest you came was {match}/5')
+        print(f'The closest you came was {match}/5 on {amtHighest} tickets.')
 else:
     week=0
-    input('Hit "enter" to start the simulation')
+    input('Hit "enter" to start the simulation...')
     while lotto!=1:
         match=0
         week+=1
         age=(18+(week/52))
-        if age<1000:
-            print(f'Games Lost:{week}\t\tYear{age:.2f}')
-        else:
-            print(f'Games Lost: {week}\tYear: {age:.2f}\tMoney Spent: ${price}\tClosest To Winning: {highestMatch}/5')
+        print(f'Year: {age:.2f}\tMoney Spent: ${price}\tClosest To Winning: {highestMatch}/5 on {amtHighest} out of {week} tickets purchased')
         price+=2
         wNumbers=winningNumbers()
         for i in range(0,5):
@@ -141,6 +157,9 @@ else:
             lotto=0
             if match>highestMatch:
                 highestMatch=match
+                amtHighest=1
+            elif match==highestMatch:
+                amtHighest+=1
 
 
     if lotto==1:
